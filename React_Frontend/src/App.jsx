d06621ede5f8ead24041c20a86cbfca4ab2b91e5
Ahire Tersoo David <tersooahire21@gmail.com>
2023-09-01 12:37:21 +0100

    created hero section

--- React_Frontend/src/App.jsx
+++ React_Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./styles/App.css";
 import Navigation from "./components/Navigation";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
   return (
--- React_Frontend/src/pages/Home.jsx
+++ React_Frontend/src/pages/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import illustration1 from "../assets/illustration1.svg";
 
 export default function Home() {
-    return (
-        <>
-            {/* Home */}
-        </>
-    )
+  return (
+    <main className="homepage">
+      <header className="hero-container">
+        <div className="hero-container-left">
+          <h1>
+            Unlock a World of{" "}
+            <span style={{ color: "gold", fontStyle: "italic" }}>Movies.</span>{" "}
+            Explore, Discover & Enjoy!
+          </h1>
+          <button>Find Movies</button>
+        </div>
+        <div className="hero-container-right">
+          <img
+            src={illustration1}
+            alt="Illustration of Woman Searching for movie"
+          />
+        </div>
+      </header>
+    </main>
+  );
 }
