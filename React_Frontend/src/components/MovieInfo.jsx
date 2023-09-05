b34c6cc7c61671cdfe033112c76889f1a7f03942
Ahire Tersoo David <tersooahire21@gmail.com>
2023-09-05 11:40:36 +0100

    create modal to display movie information

--- React_Frontend/src/components/MovieInfo.jsx
+++ React_Frontend/src/components/MovieInfo.jsx
@@ -1,9 +1,32 @@
-import React from "react";
-// import genericbanner from "../assets/generic_banner.png";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faSearchengin } from "@fortawesome/free-brands-svg-icons";
+import genericbanner from "../assets/generic_banner.png";
 import { useMovieData } from "../Context";
 
 export default function MovieInfo() {
   const { movieData } = useMovieData(); // Access the movieData State from context
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const openMovieModal = (movie) => {
+    setLoading(true);
+    let countdown = 1; // Set 1 second countdown for displaying information
+    const countdownInterval = setInterval(() => {
+      countdown--;
+
+      if (countdown === 0) {
+        clearInterval(countdownInterval);
+        setLoading(false);
+        setSelectedMovie(movie); // Show movie information once countdown ends
+      }
+    }, 1000);
+  };
+
+  const closeMovieModal = () => {
+    setSelectedMovie(null);
+  };
 
   return (
     <>
@@ -12,7 +35,11 @@ export default function MovieInfo() {
           <h2>Search Results</h2>
           {movieData.Search.map((result, index) => (
             <div className="sub-container" key={index}>
-              <div className="movie-list" title="Click to see more information">
+              <div
+                className="movie-list"
+                title="Click to see more information"
+                onClick={() => openMovieModal(result)}
+              >
                 <span className="title">{result.Title}</span>
                 <span>
                   Type:
@@ -27,6 +54,60 @@ export default function MovieInfo() {
                   <span className="value"> {result.imdbID}</span>
                 </span>
               </div>
+              {selectedMovie && (
+                <div className="movie-modal-container">
+                  <div className="information-area">
+                    <FontAwesomeIcon
+                      icon={faXmark}
+                      onClick={closeMovieModal}
+                      size="xl"
+                      className="cancel-button"
+                    />
+                    <div className="information-sub-area">
+                      <div className="image-container">
+                        <img
+                          src={
+                            selectedMovie.Poster === "N/A"
+                              ? genericbanner
+                              : selectedMovie.Poster
+                          }
+                          alt="Poster"
+                        />
+                      </div>
+                      <div className="text-container">
+                        <h4>{selectedMovie.Title}</h4>
+                        <span>
+                          Type:
+                          <span className="value"> {selectedMovie.Type}</span>
+                        </span>
+                        <span>
+                          Year:
+                          <span className="value"> {selectedMovie.Year}</span>
+                        </span>
+                        <span>
+                          IMBD Id:
+                          <span className="value"> {selectedMovie.imdbID}</span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              )}
+              {loading && (
+                <div className="movie-modal-container">
+                  <div
+                    className="information-area"
+                    style={{ alignItems: "center", justifyContent: "center" }}
+                  >
+                    <FontAwesomeIcon
+                      icon={faSearchengin}
+                      size="2xl"
+                      style={{ color: "gold", width: "75px", height: "75px" }}
+                      beat
+                    />
+                  </div>
+                </div>
+              )}
             </div>
           ))}
           <b className="results">Total Results: {movieData.Search.length}</b>
